930c2ac2b63e0ed40edfff893fedae40d40970ca
JooJC <joojmoutinho45@gmail.com>
2023-10-17 17:03:27 -0300

    add: theme light and provider

--- projeto-materialui-react-typescript/src/App.tsx
+++ projeto-materialui-react-typescript/src/App.tsx
@@ -1,11 +1,16 @@
 import {BrowserRouter} from 'react-router-dom'
 import { AppRoutes } from './routes'
+import {ThemeProvider} from '@mui/material'
+import { lightTheme } from './shared/themes'
 
 export const App = () => {
   return (
+    <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
         <AppRoutes/>
       </BrowserRouter>
+    </ThemeProvider>
+
 
 
   )
