36889e8467ef70a2ace2a2d92116a690d16eea3a
JooJC <joojmoutinho45@gmail.com>
2023-12-05 16:25:29 -0300

    deploy fix

--- projeto-materialui-react-typescript/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./pessoas/ListagemDePessoas";
-export * from "./pessoas/DetalheDePessoas";
-export * from "./cidades/DetalheDeCidades";
-export * from "./cidades/ListagemDeCidades";
-export * from "./dashboard/Dashboard";
--- projeto-materialui-react-typescript/src/shared/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./login/Login";
-export * from "./menu-lateral/MenuLateral";
-export * from "./ferramentas-de-detalhe/FerramentasDeDetalhe";
-export * from "./ferramentas-de-listagem/FerramentasDeListagem";
